dbedbdabb81c58efe78e116324a33258d1a47d3d
annannkl <anna.tzavara@hotmail.com>
2024-05-28 20:33:08 +0300

    This is another game I made. Specifically, taking a guess on a number from 1 to 100, where we have purposely have added the answer to appear in the terminal in order to confirm it is working correctly.

---
+++ src/NumberGuessing/ColoredUI.java
@@ -0,0 +1,44 @@
+package NumberGuessing;
+
+public class ColoredUI {
+    // ANSI escape code constants for text colors
+    private String RESET = "\u001B[0m"; // Reset text color
+    private String blue = "\u001B[34m"; // Set text color to blue
+    private String red = "\u001B[31m"; // Set text color to red
+    private String green = "\u001B[32m"; // Set text color to green
+    private String cyan = "\u001B[36m" ; // Set text color to cyan
+    private String purple = "\u001B[35m"; // Set text color to purple
+
+    public ColoredUI() {
+        this.RESET = RESET;
+        this.blue = blue;
+        this.red = red;
+        this.green = green;
+        this.cyan = cyan;
+        this.purple = purple;
+    }
+
+    public String getRESET() {
+        return RESET;
+    }
+
+    public String getGreen() {
+        return green;
+    }
+
+    public String getRed() {
+        return red;
+    }
+
+    public String getBlue() {
+        return blue;
+    }
+
+    public String getCyan() {
+        return cyan;
+    }
+
+    public String getPurple() {
+        return purple;
+    }
+}
---
+++ src/NumberGuessing/Game.java
@@ -0,0 +1,50 @@
+package NumberGuessing;
+
+import RockPaperScissors.ColoredUI;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+
+        ColoredUI coloredUI = new ColoredUI();
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int randomNumber = rand.nextInt(100) + 1;
+        System.out.println(coloredUI.getCyan() + "The Random Number is: " + randomNumber);
+        System.out.println("------------------------------------" + coloredUI.getRESET());
+
+        System.out.println(coloredUI.getPurple() + "------------------------------------------");
+        System.out.println("Welcome! Let's play a game, shall we? :D" + coloredUI.getRESET());
+
+        int tryCount = 0;
+        while(true) {
+            System.out.println(coloredUI.getBlue() + "------------------------------------------");
+            System.out.print("Enter your guess (1-100): " + coloredUI.getRESET());
+
+            int playerGuess = scanner.nextInt();
+            tryCount++;
+
+            if (playerGuess == randomNumber) {
+                System.out.println(coloredUI.getGreen()+ "------------------------------------");
+                System.out.println("Correct! You win!" + coloredUI.getRESET());
+                System.out.println(coloredUI.getCyan()+ "---------------------");
+                System.out.println("It took you " + tryCount + " tries!" + coloredUI.getRESET());
+                System.out.println(coloredUI.getPurple() + "------------------------------------------------");
+                System.out.println("That's all for now! Thanks for playing! Bye! :D" + coloredUI.getRESET());
+                break;
+            }
+            else if (randomNumber > playerGuess) {
+                System.out.println(coloredUI.getRed()+ "------------------------------------");
+                System.out.println("False! The number is higher! Guess again!" + coloredUI.getRESET());
+
+            }
+            else {
+                System.out.println(coloredUI.getRed()+ "------------------------------------");
+                System.out.println("False! The number is lower! Guess again!" + coloredUI.getRESET());
+            }
+        }
+    }
+}
